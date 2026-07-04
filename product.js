62eca0f50576c5d31833d35ff4c09b9eb2e0c4a9
timchepurney <chepurnoytim@gmail.com>
2026-07-03 18:49:54 -0700

    Update product.js

--- product.js
+++ product.js
@@ -1,1 +1,111 @@
+const products = [
 
+{
+id:1,
+
+name:"Foot Locks",
+
+price:15,
+
+category:"Controls",
+
+image:"images/footlocks.jpg",
+
+gallery:[
+"images/footlocks.jpg",
+"images/footlocks2.jpg",
+"images/footlocks3.jpg"
+],
+
+description:"Premium CNC foot locks built for aggressive riding and maximum grip.",
+
+fitment:[
+"Surron LBX",
+"Talaria XXX"
+],
+
+variants:[
+{
+name:"Black",
+price:15
+},
+{
+name:"Silver",
+price:15
+}
+],
+
+stock:true,
+
+featured:true
+},
+
+{
+id:2,
+
+name:"Grips",
+
+price:10,
+
+category:"Controls",
+
+image:"images/grips.jpg",
+
+gallery:[
+"images/grips.jpg"
+],
+
+description:"Soft compound grips with excellent comfort and control.",
+
+fitment:[
+"Surron",
+"Talaria"
+],
+
+variants:[],
+
+stock:true,
+
+featured:true
+},
+
+{
+id:3,
+
+name:"Aluminum Motor Cover",
+
+price:40,
+
+category:"Protection",
+
+image:"images/motorcover.jpg",
+
+gallery:[
+"images/motorcover.jpg",
+"images/motorcover2.jpg"
+],
+
+description:"CNC aluminum motor cover for added protection and premium style.",
+
+fitment:[
+"Surron LBX",
+"Talaria XXX"
+],
+
+variants:[
+{
+name:"Surron LBX",
+price:40
+},
+{
+name:"Talaria XXX",
+price:45
+}
+],
+
+stock:true,
+
+featured:true
+}
+
+];
